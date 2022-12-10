48ca7c8ab46d25881ad3f9566cfca249ee5842aa
agrawalrajat310 <agrawalrajat310@gmail.com>
2022-12-11 01:46:06 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-jsx-expressions-example-
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-jsx-expressions-example-hyvfrl)
---
+++ index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
+
+const name = 'RAJAT !!';
+const el = <p>Hello, {name}</p>;
+
+ReactDOM.render(el, document.getElementById('root'));
